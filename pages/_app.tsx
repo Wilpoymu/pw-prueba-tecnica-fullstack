672bd1d7dfc2b0adcc0a1febbd228f7f9628e6f5
Wilfer Zuluaga <wilferzuluagadev@gmail.com>
2025-10-05 22:28:08 -0500

    feat: wrap application in ThemeProvider for theme management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default App;
