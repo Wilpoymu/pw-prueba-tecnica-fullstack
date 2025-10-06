3cffc815fa839ce230b90c510d7dace81f47871d
Wilfer Zuluaga <wilferzuluagadev@gmail.com>
2025-10-05 21:52:52 -0500

    feat: add role field to user authentication configuration

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -16,6 +16,14 @@ export const auth = betterAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
   },
+  user: {
+    additionalFields: {
+      role: {
+        type: 'string',
+        required: true,
+        defaultValue: 'ADMIN',
+        returned: true,
+      },
+    },
+  },
 });
-
-export type Session = typeof auth.$Infer.Session;
